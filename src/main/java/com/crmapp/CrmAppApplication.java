2246afdf54ff3281dce1c2d315fe01c141373f64
tavodevcode <tavodevcode@gmail.com>
2023-11-27 21:19:37 -0600

    📝 Add CrmAppApplication class

---
+++ src/main/java/com/crmapp/CrmAppApplication.java
@@ -0,0 +1,12 @@
+package com.crmapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmAppApplication.class, args);
+	}
+}
