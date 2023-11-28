f2e39c1dc2bc879706997cc268bffcbe19502e36
tavodevcode <tavodevcode@gmail.com>
2023-11-27 21:19:50 -0600

    📝 Add User model class

---
+++ src/main/java/com/crmapp/models/User.java
@@ -0,0 +1,98 @@
+package com.crmapp.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false, columnDefinition = "UUID default gen_random_uuid()")
+    private UUID id;
+
+    @Column(name = "first_name", length = 150, nullable = false)
+    private String first_name;
+
+    @Column(name = "last_name", length = 150, nullable = false)
+    private String last_name;
+
+    @Column(name = "email", length = 150, nullable = false)
+    private String email;
+
+    @Column(name = "phone", length = 10, nullable = false)
+    private String phone;
+
+    @Column(name = "address", length = 150, nullable = false)
+    private String address;
+
+    // constructors
+    public User() {
+    }
+
+    public User(UUID id, String first_name, String last_name, String email, String phone, String address) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+
+    // getters and setters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+}
