45f792311e85e97d97dc380c62abf6c485625da7
tavodevcode <tavodevcode@gmail.com>
2023-11-29 23:28:35 -0600

    📝 Refactor UserController to add CRUD operations for User entity

--- src/main/java/com/crmapp/controllers/UserController.java
+++ src/main/java/com/crmapp/controllers/UserController.java
@@ -1,13 +1,24 @@
 package com.crmapp.controllers;
 
+import java.util.UUID;
+
+import javax.validation.Valid;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.crmapp.dtos.UserCreateDTO;
+import com.crmapp.models.User;
 import com.crmapp.services.UserService;
 
 @RestController
@@ -33,6 +44,39 @@ public class UserController {
 
         Pageable pageable = PageRequest.of(page - 1, size); // subtract 1 from page
 
-        return this.userService.getUsers(query, first_name, last_name, email, phone, address, pageable);
+        return this.userService.getUsers(query, first_name, last_name, email, phone,
+                address, pageable);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Object> createUser(@Valid @RequestBody UserCreateDTO userCreateDTO) {
+        User user = new User();
+
+        user.setFirst_name(userCreateDTO.getFirst_name());
+        user.setLast_name(userCreateDTO.getLast_name());
+        user.setEmail(userCreateDTO.getEmail());
+        user.setPhone(userCreateDTO.getPhone());
+        user.setAddress(userCreateDTO.getAddress());
+
+        return this.userService.createUser(user);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> updateUser(@Valid @RequestBody UserCreateDTO userCreateDTO,
+            @PathVariable("id") UUID id) {
+        User user = new User();
+
+        user.setFirst_name(userCreateDTO.getFirst_name());
+        user.setLast_name(userCreateDTO.getLast_name());
+        user.setEmail(userCreateDTO.getEmail());
+        user.setPhone(userCreateDTO.getPhone());
+        user.setAddress(userCreateDTO.getAddress());
+
+        return this.userService.updateUser(user, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable("id") UUID id) {
+        return this.userService.deleteUser(id);
     }
 }
