25462a3f0437aab6950e87ee16285c379fd8747b
tavodevcode <tavodevcode@gmail.com>
2023-11-29 23:28:50 -0600

    📝 Add GlobalExceptionHandler class for handling validation exceptions

---
+++ src/main/java/com/crmapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.crmapp.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
